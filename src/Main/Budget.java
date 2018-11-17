0d7aafc954e01330de5ca79c5b300dcc1d6a662d
Ruiqi Wang <ruiqi.wang@mail.utoronto.ca>
2018-11-17 12:40:22 -0500

    moved files

---
+++ src/Main/Budget.java
@@ -0,0 +1,17 @@
+public enum Budget {
+    housing(6),
+    living(6),
+    busFare(2.0),
+    subwayFare(0.5),
+    weeklyPass(40),
+    monthlyPass(100),
+    newCard(9.99),
+    cardInitValue(19);
+
+    private double budget;
+
+    Budget(double budget) {
+
+    }
+
+}
